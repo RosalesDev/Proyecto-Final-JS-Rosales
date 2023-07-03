dfab09af0f5e587a61c59fd6b62f255a344a56f4
Sebastian Rosales <srosales.dev@gmail.com>
2023-07-02 22:24:53 -0300

    Se cambio la forma en que se renderiza la tabla con los turnos por profesional

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Proyecto-Final-JS-Rosales
 Proyecto final del curso de JavaScript de Coderhouse.
+
+Es un prototipo para un gestor de turnos de una clínica médica.
+Se que faltaría implementar muchas funcionalidades pero el objetivo era aplicar lo que se aprendió en el curso.
--- script/script-appointment-page.js
+++ script/script-appointment-page.js
@@ -1,62 +1,60 @@
-const accordion = document.querySelector('.accordion');
-const accordionContainer = document.querySelector('.container');
-const appointmentList = JSON.parse(localStorage.getItem('appointment_list'));
+const accordion = document.querySelector(".accordion");
+const accordionContainer = document.querySelector(".container");
+const appointmentList = JSON.parse(localStorage.getItem("appointment_list"));
 
-if(appointmentList.length > 0) {
-  
+if (appointmentList.length > 0) {
   let appointmentMap = new Map();
   let professionalIdList = [];
   let patientList = [];
 
-/* ------- CREO UN MAP CON LOS ID DE LOS PROFESIONALES Y UNA LISTA
+  /* ------- CREO UN MAP CON LOS ID DE LOS PROFESIONALES Y UNA LISTA
             PARA SUS PACIENTES                                    ------ */
-  for(let appointment of appointmentList){
-
+  for (let appointment of appointmentList) {
     let professionalId = appointment.professional_planning.professional.id;
 
-    appointmentMap.set(professionalId,[]);
+    appointmentMap.set(professionalId, []);
   }
 
   professionalIdList = [...appointmentMap.keys()];
-  
-/* --- OBTENGO LOS PACIENTES DE CADA PROFESIONAL Y SE LOS ASIGNO EN EL MAP -- */
-  for(let professionalId of professionalIdList){
-      patientList = appointmentList.filter(appointment => 
-      appointment.professional_planning.professional.id == professionalId
+
+  /* --- OBTENGO LOS PACIENTES DE CADA PROFESIONAL Y SE LOS ASIGNO EN EL MAP -- */
+  for (let professionalId of professionalIdList) {
+    patientList = appointmentList.filter(
+      (appointment) =>
+        appointment.professional_planning.professional.id == professionalId
     );
-    appointmentMap.set(professionalId,patientList);
+    appointmentMap.set(professionalId, patientList);
   }
 
 /* -------- SE GENERA LA TABLA CON LOS PROFESIONALES Y SUS PACIENTES -------- */
-  for(let appointment of appointmentMap) {
 
-    let professionalId = appointment[0];
-    let currentAppointment = appointment[1][0];
+  for (let currentProfessionalId of professionalIdList) {
+    let appointmentList = appointmentMap.get(currentProfessionalId);
+    let currentProfessional = appointmentList[0].professional_planning.professional;
 
-    function buildTableBody(appointmentList){
-      let tableBody = '';
+    const profesionalLastName = currentProfessional.person.last_name;
+    const profesionalFirstName = currentProfessional.person.first_name;
+    const profesionalSpecialty = currentProfessional.specialty;
+
+    function buildTableBody(appointmentList) {
+      let tableBody = "";
       let personIndex = 1;
 
-      for(let appointment of appointmentList){
+      for (let appointment of appointmentList) {
         let person = appointment.person;
-          tableBody += `
+        tableBody += `
           <tr>
             <th scope="row">${personIndex}</th>
             <td>${appointment.time}</td>
             <td>${person.doc_number}</td>
             <td>${person.last_name}</td>
             <td>${person.first_name}</td>
-          </tr>`
-          personIndex++;
+          </tr>`;
+        personIndex++;
       }
       return tableBody;
     }
-
-
-    const profesionalLastName = currentAppointment.professional_planning.professional.person.last_name;
-    const profesionalFirstName = currentAppointment.professional_planning.professional.person.first_name;
-    const profesionalSpecialty = currentAppointment.professional_planning.professional.specialty;
-
+    
     const accordionItem = document.createElement("div");
     accordionItem.className = 'accordion-item';
     accordionItem.innerHTML = `
@@ -66,16 +64,16 @@ if(appointmentList.length > 0) {
           class="accordion-button"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#collapse${professionalId}"
+          data-bs-target="#collapse${currentProfessionalId}"
           aria-expanded="true"
-          aria-controls="collapse${professionalId}"
+          aria-controls="collapse${currentProfessionalId}"
         >
           ${profesionalLastName} ${profesionalFirstName}
           (${profesionalSpecialty})
         </button>
       </h2>
       <div
-        id="collapse${professionalId}"
+        id="collapse${currentProfessionalId}"
         class="accordion-collapse collapse"
         data-bs-parent="#accordionExample"
       >
@@ -91,19 +89,16 @@ if(appointmentList.length > 0) {
               </tr>
             </thead>
             <tbody>
-            ${buildTableBody(appointment[1])}
+            ${buildTableBody(appointmentList)}
             </tbody>
           </table>
         </div>
       </div>
-    </div>
-  `;
-
+    </div>`;
+    
     accordion.append(accordionItem);
   }
-
-}
-else{
+} else {
   accordion.remove();
   accordionContainer.innerHTML = `
   <div class="container text-center pt-5"><h5>No hay turnos registrados</h5></div>
